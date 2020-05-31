4e56ec25aeb16807fed4014d84a033a17692dd80
Kirill Shamilin <k.shamilin@gmail.com>
2020-05-31 16:51:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,19 +36,21 @@ def run_command(command):
 
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Привет, я бот, жду команды")
+    bot.sendMessage(chat_id=update.message.chat_id, text="Приветики, меня зовут Centa, я твой покорный бот, жду команды")
 
 
 def help(bot, update):
     reload(config)
     bot.sendMessage(chat_id=update.message.chat_id, text='''список доступных команд:
-    /status- состояние сервера ts3
-    /restart - перезапуск ts3
-    /update - обновление сервера ts3
-    /backup - резервная копия сервера ts3
-    /db_backup - резервная копия базы данных сервера ts3
+    /id - узнать свой Telegram id
+    /ts3status- состояние сервера TS3
+    /ts3restart - перезапуск сервера TS3
+    /ts3start - запуск сервера TS3
+    /ts3stop - остановка сервера TS3
+    /ts3update - обновление сервера TS3
+    /ts3backup - резервная копия сервера TS3
+    /db_backup - резервная копия базы данных сервера TS3
     /freemem - информация о памяти
-    /cpustat - информация о нагрузке на процессор
     /df - информация о наличии места на жетском диске
     /dirspace - объем папки''' + config.dir1 + '''
     /bkpspace - размер файла бэкапа за текущий день в папке ''' + config.dir_backup + '''
@@ -60,15 +62,6 @@ def myid(bot, update):
     userid = update.message.from_user.id
     bot.sendMessage(chat_id=update.message.chat_id, text=userid)
 
-
-#функция команады ifconfig
-def ifconfig(bot, update):
-    reload(config)
-    user = str(update.message.from_user.id)
-    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
-        run_command("ip a")
-        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
-
 #функция команады df
 def df(bot, update):
     reload(config)
@@ -77,28 +70,12 @@ def df(bot, update):
         run_command("df -h")
         bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
 
-#функция команады free
+#функция команады freemem
 def freemem(bot, update):
     reload(config)
     user = str(update.message.from_user.id)
     if user in config.admin: #если пользовательский id в списке admin то команда выполняется
         run_command("free -m")
-        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
-
-#функция команады mpstat
-def mpstat(bot, update):
-    reload(config)
-    user = str(update.message.from_user.id)
-    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
-        run_command("systemctl status controlbot")
-        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
-
-#функция команады top
-def apachestatus(bot, update):
-    reload(config)
-    user = str(update.message.from_user.id)
-    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
-        run_command("apachestatus")
         bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
 
 #функция команады dir1
@@ -130,19 +107,63 @@ def dirbackup(bot, update):
         run_command(filebackup_command)
         bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
 
-def reboot(bot, update):
+#функция команады ts3status
+def ts3status(bot, update):
     reload(config)
     user = str(update.message.from_user.id)
-    if user in config.admin:
-        run_command("uname -a")
+    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
+        ts3status_command = config.dir1+"ts3server status"
+        run_command(ts3status_command)
         bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
-
-
+        
+#функция команады ts3restart
+def ts3restart(bot, update):
+    reload(config)
+    user = str(update.message.from_user.id)
+    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
+        ts3restart_command = config.dir1+"ts3server restart"
+        run_command(ts3restart_command)
+        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
+        
+#функция команады ts3update
+def ts3update(bot, update):
+    reload(config)
+    user = str(update.message.from_user.id)
+    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
+        ts3update_command = config.dir1+"ts3server update"
+        run_command(ts3update_command)
+        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
+        
+#функция команады ts3backup
+def ts3backup(bot, update):
+    reload(config)
+    user = str(update.message.from_user.id)
+    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
+        ts3backup_command = config.dir1+"ts3server backup"
+        run_command(ts3backup_command)
+        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
+        
+#функция команады ts3stop
+def ts3stop(bot, update):
+    reload(config)
+    user = str(update.message.from_user.id)
+    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
+        ts3stop_command = config.dir1+"ts3server stop"
+        run_command(ts3stop_command)
+        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
+        
+#функция команады ts3start
+def ts3start(bot, update):
+    reload(config)
+    user = str(update.message.from_user.id)
+    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
+        ts3start_command = config.dir1+"ts3server start"
+        run_command(ts3start_command)
+        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)        
+        
+        
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-
-ifconfig_handler = CommandHandler('ifconfig', ifconfig)
-dispatcher.add_handler(ifconfig_handler)
 
 df_handler = CommandHandler('df', df)
 dispatcher.add_handler(df_handler)
@@ -150,26 +171,34 @@ dispatcher.add_handler(df_handler)
 freemem_handler = CommandHandler('freemem', freemem)
 dispatcher.add_handler(freemem_handler)
 
-mpstat_handler = CommandHandler('mpstat', mpstat)
-dispatcher.add_handler(mpstat_handler)
-
-apachestatus_handler = CommandHandler('apachestatus', apachestatus)
-dispatcher.add_handler(apachestatus_handler)
-
 dir1_handler = CommandHandler('dir1', dir1)
 dispatcher.add_handler(dir1_handler)
 
 dirbackup_handler = CommandHandler('dirbackup', dirbackup)
 dispatcher.add_handler(dirbackup_handler)
 
+ts3status_handler = CommandHandler('ts3status', ts3status)
+dispatcher.add_handler(ts3status_handler)
+
+ts3restart_handler = CommandHandler('ts3restart', ts3restart)
+dispatcher.add_handler(ts3restart_handler)
+
+ts3update_handler = CommandHandler('ts3update', ts3update)
+dispatcher.add_handler(ts3update_handler)
+
+ts3backup_handler = CommandHandler('ts3backup', ts3backup)
+dispatcher.add_handler(ts3backup_handler)
+
+ts3stop_handler = CommandHandler('ts3stop', ts3stop)
+dispatcher.add_handler(ts3stop_handler)
+
+ts3start_handler = CommandHandler('ts3start', ts3start)
+dispatcher.add_handler(ts3start_handler)
+
 myid_handler = CommandHandler('id', myid)
 dispatcher.add_handler(myid_handler)
 
 help_handler = CommandHandler('help', help)
 dispatcher.add_handler(help_handler)
-
-reboot_handler = CommandHandler('reboot', reboot)
-dispatcher.add_handler(reboot_handler)
-
 
 updater.start_polling()
