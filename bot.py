be46d1107a24e2fbd578872de232c8811d8a18a8
Kirill Shamilin <k.shamilin@gmail.com>
2020-05-31 18:15:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,11 +49,7 @@ def help(bot, update):
     /ts3stop - остановка сервера TS3
     /ts3update - обновление сервера TS3
     /ts3backup - резервная копия сервера TS3
-    /db_backup - резервная копия базы данных сервера TS3
-    /freemem - информация о памяти
     /df - информация о наличии места на жетском диске
-    /dirspace - объем папки''' + config.ts3dir + '''
-    /bkpspace - размер файла бэкапа за текущий день в папке ''' + config.dir_backup + '''
 
     ''')
 
@@ -68,43 +64,6 @@ def df(bot, update):
     user = str(update.message.from_user.id)
     if user in config.admin: #если пользовательский id в списке admin то команда выполняется
         run_command("df -h")
-        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
-
-#функция команады freemem
-def freemem(bot, update):
-    reload(config)
-    user = str(update.message.from_user.id)
-    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
-        run_command("free -m")
-        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
-
-#функция команады dir1
-def dir1(bot, update):
-    reload(config)
-    user = str(update.message.from_user.id)
-    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
-        dir1_command = "du -sh "+ config.ts3dir
-        run_command(dir1_command)
-        bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
-
-#функция команады dirbackup - проверяет наличие файла по дате
-def dirbackup(bot, update):
-    reload(config)
-    user = str(update.message.from_user.id)
-    if user in config.admin: #если пользовательский id в списке admin то команда выполняется
-        now_date = datetime.date.today() # Текущая дата
-        cur_year = str(now_date.year) # Год текущий
-        cur_month = now_date.month # Месяц текущий
-        if cur_month < 10:
-            cur_month = str(now_date.month)
-            cur_month = '0'+ cur_month
-        else:
-            cur_month = str(now_date.month)
-        cur_day = str(now_date.day) # День текущий
-        filebackup = config.dir_backup + cur_year + '-' + cur_month + '-' + cur_day + '.03.00.co.7z'  #формируем имя файла для поиска
-        print (filebackup)
-        filebackup_command = "ls -lh "+ filebackup
-        run_command(filebackup_command)
         bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
 
 #функция команады ts3status
@@ -165,17 +124,14 @@ def ts3start(bot, update):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+myid_handler = CommandHandler('id', myid)
+dispatcher.add_handler(myid_handler)
+
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
+
 df_handler = CommandHandler('df', df)
 dispatcher.add_handler(df_handler)
-
-freemem_handler = CommandHandler('freemem', freemem)
-dispatcher.add_handler(freemem_handler)
-
-dir1_handler = CommandHandler('dir1', dir1)
-dispatcher.add_handler(dir1_handler)
-
-dirbackup_handler = CommandHandler('dirbackup', dirbackup)
-dispatcher.add_handler(dirbackup_handler)
 
 ts3status_handler = CommandHandler('ts3status', ts3status)
 dispatcher.add_handler(ts3status_handler)
@@ -194,11 +150,5 @@ dispatcher.add_handler(ts3stop_handler)
 
 ts3start_handler = CommandHandler('ts3start', ts3start)
 dispatcher.add_handler(ts3start_handler)
-
-myid_handler = CommandHandler('id', myid)
-dispatcher.add_handler(myid_handler)
-
-help_handler = CommandHandler('help', help)
-dispatcher.add_handler(help_handler)
 
 updater.start_polling()
